e6ddda77d98d20a5120950cf12cbf3a82ebacc80
gafayo <gafayoabel20@gmail.com>
2022-12-22 02:42:13 +0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
  const toggleBtn = document.getElementsByClassName('toggle-button')[0];
  const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
+ const handles = document.querySelectorAll('.faq__entity');
+ const paras = document.querySelectorAll('.faq__para');
+
  toggleBtn.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
  })
- console.log(navbarLinks);
+ 
+
+//  faq starts from here
+handles.forEach(handle => handle.addEventListener('click', function(){
+   paras.forEach(para => para.classList.toggle('open'))
+}));
